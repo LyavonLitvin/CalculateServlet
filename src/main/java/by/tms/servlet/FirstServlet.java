6e4ccccd629b33d6301a3109640236da36ca177e
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-01-21 12:45:54 +0300

    add NullPointerException

--- src/main/java/by/tms/servlet/FirstServlet.java
+++ src/main/java/by/tms/servlet/FirstServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 import calculator.*;
 import calculator.validator.Validator;
 
@@ -14,22 +15,24 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, NullPointerException {
         Validator validator = new Validator();
         Calculator calculator = new Calculator();
-        String firstNumberString = req.getParameter("num1");
-        String secondNumberString = req.getParameter("num2");
-        String operationType = req.getParameter("opType");
-        if (validator.isNumeric(firstNumberString) && validator.isNumeric(secondNumberString) && validator.isRightOperator(operationType)) {
-            double firstNumber = Double.parseDouble(firstNumberString);
-            double secondNumber = Double.parseDouble(secondNumberString);
-            Result result = new Result();
-            result.setFirstNumber(firstNumber);
-            result.setSecondNumber(secondNumber);
-            result.setOperatorType(operationType);
-            calculator.calculation(result);
-            resp.getWriter().println(result);
-        } else {
-            resp.getWriter().println("Wrong enter numbers or opType");
+        try {
+            String firstNumberString = req.getParameter("num1");
+            String secondNumberString = req.getParameter("num2");
+            String operationType = req.getParameter("opType");
+            if (validator.isNumeric(firstNumberString) && validator.isNumeric(secondNumberString) && validator.isRightOperator(operationType)) {
+                double firstNumber = Double.parseDouble(firstNumberString);
+                double secondNumber = Double.parseDouble(secondNumberString);
+                Result result = new Result();
+                result.setFirstNumber(firstNumber);
+                result.setSecondNumber(secondNumber);
+                result.setOperatorType(operationType);
+                calculator.calculation(result);
+                resp.getWriter().println(result);
+            } else {
+                resp.getWriter().println("Wrong enter numbers or opType");
+            }
+        } catch (NullPointerException e) {
+            resp.getWriter().println("One of the fields is empty");
         }
-
-
     }
 }
